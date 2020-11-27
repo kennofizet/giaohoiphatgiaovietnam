d4af11973325ad20a3d4eaece0c254adf8982174
unknown <phuonglhkmhd123@gmail.com>
2020-11-27 16:06:45 +0700

    css

--- resources/views/admin/official/index.blade.php
+++ resources/views/admin/official/index.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.layouts.main')
 @section('style')
-<link href="{{url('/')}}/admin/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+<link href="/admin/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 @endsection
 
 @section('content')
@@ -86,8 +86,8 @@
 
 @section('script')
  <!-- Page level plugins -->
-    <script src="{{url('/')}}/admin/vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="{{url('/')}}/admin/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+    <script src="/admin/vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="/admin/vendor/datatables/dataTables.bootstrap4.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
     <!-- Page level custom scripts -->
